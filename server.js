9a65f89b7865c273678fdecae0f48bf2edb65d54
dymaksakov <dymaksakov@avito.ru>
2024-03-13 16:44:33 +0300

    изменены параметры сборки

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ const PORT = 3000;
 
 app.use(express.static('./dist'));
 
+app.get('/', (req, res) => {
+    res.status(200)
+    res.send('Hello World!');
+});
+
 app.listen(PORT, function () {
     console.log(`Example app listening on port ${PORT}!`);
 });

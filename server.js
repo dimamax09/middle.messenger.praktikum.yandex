fd0d71357bee374d6b35237d76f84bbd1a325fbb
dymaksakov <dymaksakov@avito.ru>
2024-03-20 21:21:04 +0300

    добавлен esLint

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
 const PORT = 3000;
@@ -6,10 +6,10 @@ const PORT = 3000;
 app.use(express.static('./dist'));
 
 app.get('/', (req, res) => {
-    res.status(200)
-    res.send('Hello World!');
+  res.status(200);
+  res.send('Hello World!');
 });
 
-app.listen(PORT, function () {
-    console.log(`Example app listening on port ${PORT}!`);
+app.listen(PORT, () => {
+  // console.log(`Example app listening on port ${PORT}!`);
 });
